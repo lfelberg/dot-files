d9044a4966bddc1e4d4699c6984eda0d8fb2a230
Hack Reactor Student <student@hackreactor.com>
2018-09-28 11:59:34 -0700

    Adding in an ESLint dot file

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+    "extends": "airbnb-base",
+      "rules": {
+         "no-unused-vars": "warn",
+         "no-shadow": "warn",
+         "no-prototype-builtins": "warn",
+         "no-unneeded-ternary": "warn",
+         "no-restricted-globals": "warn",
+      },
+};
